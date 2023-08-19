83b4ef75f2f780aef57781afe35a17e44d25e82a
lrhaire <lauren@laurenrivera.com>
2023-08-18 20:20:11 -0500

    discovered whitespace issue.  resolved.

--- src/components/Intro1.js
+++ src/components/Intro1.js
@@ -1,13 +1,13 @@
 function Intro1() {
-        return (
-            <div className="blog-post-intro">
-                <h2>I've become a React developer!</h2>
-                <div>
-                    <p>I've completed the React Basics course and I'm happy to announce that I'm now a Junior React Developer!</p>
-                    <p className="link">Read more...</p>
-                </div>
+  return (
+        <div className="blog-post-intro">
+            <h2>I've become a React developer!</h2>
+            <div>
+                <p>I've completed the React Basics course and I'm happy to announce that I'm now a Junior React Developer!</p>
+                <p className="link">Read more...</p>
             </div>
-        );
-    };
-    
-    export default Intro1;
+        </div>
+    );
+};
+
+export default Intro1;
